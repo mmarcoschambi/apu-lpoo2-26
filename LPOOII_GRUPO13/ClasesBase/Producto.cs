b9bbeb802622d539614c50e72254303925e82f0b
mmarcoschambi <marcos02ch@gmail.com>
2026-09-11 16:45:58 -0300

    feat: implementar item 1 con clases de dominio en ClasesBase

---
+++ LPOOII_GRUPO13/ClasesBase/Producto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Producto
+    {
+        public string CodProducto { get; set; }
+        public string Categoría { get; set; }
+        public string Color { get; set; }
+        public string Descripción { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ LPOOII_GRUPO13/ClasesBase/Proveedor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Proveedor
+    {
+        public string CUIT { get; set; }
+        public string RazonSocial { get; set; }
+        public string Domicilio { get; set; }
+        public string Telefono { get; set; }
+    }
+}
